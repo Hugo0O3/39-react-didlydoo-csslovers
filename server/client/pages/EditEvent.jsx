6624902a6321bf0220db03e44fac74393557bca0
Hugo0O3 <hugomancini2.0@gmail.com>
2025-02-10 14:45:21 +0100

    aie aie aie le post

--- server/client/pages/EditEvent.jsx
+++ server/client/pages/EditEvent.jsx
@@ -1,25 +1,65 @@
-import React, {useEffect, useState} from 'react'
-import axios from "axios"
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
+export default function App() {
+    const [postData, setPostData] = useState(null)
+    const [name, setName] = useState("")
+    const [dates, setDates] = useState([])
+    const [author, setAuthor] = useState("")
+    const [description, setDescription] = useState("")
 
-const EditEvent = () => {
-    const [eventName, setEventName] = useState('')
-    const [eventDate, setEventDate] = useState('')
-    const [eventDescription, setEventDescription] = useState('')
-    const [eventAuthor, setEventAuthor] = useState('')
+    const handleDateChange = (event) => {
+        setDates([event.target.value]); // Stocker la date dans un tableau
+    };
 
-    const eventData = {
-        name: eventName,
-        date: eventDate,
-        description: eventDescription,
-        author: eventAuthor
-    }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const eventInfos = {
+            name,
+            dates,
+            author,
+            description,
+        };
 
+        try {
+            const response = await axios.post('http://localhost:3000/api/events', eventInfos, {
+                headers: { 'Content-Type': 'application/json' }
+            });
+            console.log('Success:', response.data);
+
+            setName('')
+            setDates([])
+            setAuthor('')
+            setDescription('')
+        } catch (error) {
+            console.error('Error submitting event:', error);
+        }
+    };
 
     return (
-    <h1>All events:</h1>
+        <section className='addEvent'>
+            <form onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input type="text" value={name} onChange={(event) => setName(event.target.value)} placeholder='John Doe' required />
 
-)
-};
+                <label>Dates</label>
+                <input type="date" onChange={handleDateChange} required />
 
-export default EditEvent;
+                <label>Author</label>
+                <input type="text" value={author} onChange={(event) => setAuthor(event.target.value)} placeholder='Harry PotDeFleur' required />
+
+                <label>Description</label>
+                <textarea
+                    name="description"
+                    rows="5"
+                    placeholder="Description"
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                    required
+                />
+
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
+    )
+}
