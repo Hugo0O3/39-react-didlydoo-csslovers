cb27470f63dca85052cc1a1b84ea663299f66e78
JustineMi <michel.justine@outlook.com>
2025-02-11 10:25:14 +0100

    Co-authored-by: Hugo0O3 <Hugo0O3@users.noreply.github.com>

--- server/client/pages/Index.jsx
+++ server/client/pages/Index.jsx
@@ -1,7 +1,11 @@
 import { Outlet, Link } from "react-router-dom";
 import style from '../src/index.module.css'
-import Logo from '../src/assets/logo.png'
+import Logo from '../src/assets/logo3.png'
 import { useEffect } from "react";
+
+//import Squares from './Squares';
+import Squares from './Squares.jsx';
+
 
 
 const Index = () => {
@@ -9,26 +13,34 @@ const Index = () => {
 
     return (
         <>
-        <header>
-        <img className="didlydoo" src={Logo} alt="logo Didlydoo" />
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/events/:id">Event</Link>
-                    </li>
-                    <li>
-                        <Link to="/editEvent">EditEvent</Link>
-                    </li>
-                    <li>
-                        <Link to="/attendees">Attendees</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Outlet />
+            <header>
+                <img className={style.didlydoo} src={Logo} alt="logo Didlydoo" />
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/events/:id">Event</Link>
+                        </li>
+                        <li>
+                            <Link to="/editEvent">EditEvent</Link>
+                        </li>
+                        <li>
+                            <Link to="/attendees">Attendees</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Outlet />
             </header>
+            <Squares
+                speed={0.5}
+                squareSize={40}
+                direction='down' // up, down, left, right, diagonal
+                borderColor='#fff'
+                hoverFillColor='#222'
+            />
+
         </>
     )
 };
